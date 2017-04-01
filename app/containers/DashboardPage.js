93ae7c60d31fa9093a817d43cada3a1c4e98c2ff
Jackson Zhang <zbcjackson@gmail.com>
2017-04-01 16:02:01 +0800

    Add dashboard page

---
+++ app/containers/DashboardPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class DashboardPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          粉丝营销的神秘武器
+        </h1>
+      </div>
+    )
+  }
+}
