77f3ad3e54c7f769f520e9893a6d306fe8f2c05b
Jackson Zhang <zbcjackson@gmail.com>
2017-05-20 22:20:58 +0800

    Rename wrong account plural form

--- app/router.js
+++ app/router.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import {Router, Route, IndexRedirect, browserHistory, hashHistory} from 'react-router'
+import {Router, Route, IndexRedirect} from 'react-router'
 import {syncHistoryWithStore} from 'react-router-redux'
-import requireAuth from './authentication'
 import Authentication from './containers/Authentication'
 import history from './history'
 import SignInPage from './containers/SignInPage'
 import Page from './containers/Page'
 import DashboardPage from './containers/DashboardPage'
 import AccountsPage from './containers/AccountsPage'
-import AddAccountsPage from './containers/AddAccountPage'
+import AddAccountPage from './containers/AddAccountPage'
 
 export default ({store}) => {
   const enhancedHistory = syncHistoryWithStore(history, store)
@@ -20,7 +19,7 @@ export default ({store}) => {
           <IndexRedirect to="dashboard"/>
           <Route path="dashboard" component={DashboardPage}/>
           <Route path="accounts" component={AccountsPage}/>
-          <Route path="accounts/add" component={AddAccountsPage}/>
+          <Route path="accounts/add" component={AddAccountPage}/>
         </Route>
       </Route>
     </Router>
