d6bfa79d668880ca0513b5971f81b1908dfe750d
vani <zhaozhaowa@126.com>
2017-06-08 17:25:09 +0800

    update budget if month exists alread

--- app/actions/budget.js
+++ app/actions/budget.js
@@ -1,4 +1,4 @@
-import {fetchBudgets, createBudget, ADD_BUDGET_SUCCESS} from './budget.generated'
+import {fetchBudgets, createBudget, updateBudget, ADD_BUDGET_SUCCESS, UPDATE_BUDGET_SUCCESS} from './budget.generated'
 
 export function loadBudgets() {
   return (dispatch, getState) => {
@@ -10,6 +10,16 @@ export const addBudget = (budget, success) => {
   return (dispatch, getState) => {
     dispatch(createBudget(budget)).then(action => {
       if (action.type == ADD_BUDGET_SUCCESS){
+        success()
+      }
+    })
+  }
+}
+
+export const updateAddedBudget = (budget, success) => {
+  return (dispatch, getState) => {
+    dispatch(updateBudget(budget)).then(action => {
+      if (action.type == UPDATE_BUDGET_SUCCESS){
         success()
       }
     })
--- app/containers/AddBudgetPage.js
+++ app/containers/AddBudgetPage.js
@@ -5,8 +5,9 @@ import {bindActionCreators} from 'redux'
 import merge from 'lodash/merge'
 import * as NavigationActions from '../actions/navigation'
 import * as BudgetActions from '../actions/budget'
+import values from 'lodash/values'
 
-const mapStateToProps = () => ({})
+const mapStateToProps = (state) => ({ budgets: values(state.entities.budgets)})
 const mapDispatchToProps = (dispatch) => bindActionCreators(merge({}, BudgetActions, NavigationActions), dispatch)
 
 @connect(mapStateToProps, mapDispatchToProps)
@@ -14,6 +15,14 @@ export default class AddBudgetPage extends React.Component {
   save(){
     const month = this.refs.month.getValue()
     const amount = this.refs.amount.getValue()
+    const {budgets} = this.props
+    for(let index in budgets){
+      if(budgets[index].month === month){
+        budgets[index].amount = amount
+        this.props.updateAddedBudget(budgets[index], this.props.goBack)
+        return;
+      }
+    }
     this.props.addBudget({month, amount}, this.props.goBack)
   }
   render() {
