a9e564f7918abd8ab51780fdad7c8827c1b55e92
Yorry <i@yorry.cn>
2017-06-08 17:42:35 +0800

    req2

--- app/containers/AddBudgetPage.js
+++ app/containers/AddBudgetPage.js
@@ -11,19 +11,71 @@ const mapDispatchToProps = (dispatch) => bindActionCreators(merge({}, BudgetActi
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class AddBudgetPage extends React.Component {
-  save(){
-    const month = this.refs.month.getValue()
-    const amount = this.refs.amount.getValue()
-    this.props.addBudget({month, amount}, this.props.goBack)
+  constructor() {
+    super();
+    this.state = {
+      form: {
+        month: {
+          value: '',
+          isValid: false
+        },
+        amount: {
+          value: '',
+          isValid: false
+        }
+      }
+    }
   }
+
+  handleValueChange(field, value) {
+    let {form} = this.state;
+    let obj = {value, isValid: false}
+
+    switch (field) {
+      case 'month':
+        if ((/^\d{4}-\d{2}$/g).test(value)) {
+          obj = {
+            value,
+            isValid: true
+          }
+        }
+        break
+      case 'amount':
+        if (value > 0) {
+          obj = {
+            value,
+            isValid: true
+          }
+        }
+        break
+    }
+    this.setState({
+      form: {
+        ...form,
+        [field]: obj
+      }
+    })
+  }
+
+  save() {
+    const {form:{month, amount}} = this.state
+    if (month.isValid && amount.isValid) {
+      this.props.addBudget({month, amount}, this.props.goBack)
+    } else {
+      alert('数据不对')
+    }
+  }
+
   render() {
     const {goBack} = this.props
     return (
       <Card>
         <CardTitle title='Add Budget'/>
         <CardText>
-          <TextField fullWidth={true} id="month" ref="month" hintText="Month" floatingLabelText="Month" autoFocus />
-          <TextField fullWidth={true} id="amount" ref="amount" hintText="Amount" floatingLabelText="Amount" />
+          <TextField fullWidth={true} id="month" ref="month" hintText="Month" floatingLabelText="Month" autoFocus
+                     onChange={(e) => this.handleValueChange("month", e.target.value)}/>
+          <TextField fullWidth={true} id="amount" ref="amount" hintText="Amount" floatingLabelText="Amount"
+                     onChange={(e) => this.handleValueChange("amount", e.target.value)}/>
         </CardText>
         <CardActions>
           <RaisedButton
