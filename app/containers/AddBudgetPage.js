b91772df05b1961610cb9c0eec08e6c6d81628fa
abel yang <yanghw1017@126.com>
2017-06-08 17:41:42 +0800

    add invalid

--- app/containers/AddBudgetPage.js
+++ app/containers/AddBudgetPage.js
@@ -6,6 +6,7 @@ import merge from 'lodash/merge'
 import * as NavigationActions from '../actions/navigation'
 import * as BudgetActions from '../actions/budget'
 import values from 'lodash/values'
+import moment from 'moment'
 
 const mapStateToProps = (state) => ({ budgets: values(state.entities.budgets)})
 const mapDispatchToProps = (dispatch) => bindActionCreators(merge({}, BudgetActions, NavigationActions), dispatch)
@@ -16,14 +17,30 @@ export default class AddBudgetPage extends React.Component {
     const month = this.refs.month.getValue()
     const amount = this.refs.amount.getValue()
     const {budgets} = this.props
-    for(let index in budgets){
-      if(budgets[index].month === month){
-        budgets[index].amount = amount
-        this.props.updateAddedBudget(budgets[index], this.props.goBack)
-        return;
+    if (this.validateMonth(month) &&  this.validateAmount(amount)) {
+      for(let index in budgets){
+        if(budgets[index].month === month){
+          budgets[index].amount = amount
+          this.props.updateAddedBudget(budgets[index], this.props.goBack)
+          return;
+        }
       }
-    }
-    this.props.addBudget({month, amount}, this.props.goBack)
+      this.props.addBudget({month, amount}, this.props.goBack);
+   }
+  }
+
+  validateMonth (month) {
+    let result = month ? true: false;
+    result = moment(month, 'YYYY-MM',true).isValid();
+    return result;
+  }
+
+  validateAmount (amount) {
+    let isValid = false;
+     if (amount && amount.match(/^[1-9]\d*$/)) {
+       isValid = true;
+     }
+    return isValid;
   }
   render() {
     const {goBack} = this.props
