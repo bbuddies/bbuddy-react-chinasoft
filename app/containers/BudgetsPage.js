57ab4ac2cb9f80efa24d542f8cb288908e92a36f
wangbo <xiyou.wangbo@163.com>
2017-06-09 09:09:30 +0800

    add search function for range amount

--- app/containers/BudgetsPage.js
+++ app/containers/BudgetsPage.js
@@ -4,18 +4,57 @@ import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import merge from 'lodash/merge'
 import values from 'lodash/values'
+import findIndex from 'lodash/findIndex'
+import moment from 'moment'
 import * as NavigationActions from '../actions/navigation'
 import * as BudgetActions from '../actions/budget'
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class BudgetsPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total:0
+    }
+  }
   componentWillMount(){
     this.props.loadBudgets()
+  }
+  caculate(){
+    let {total} = this.state;
+    let startDay = this.refs.startDay.value
+    let endDay = this.refs.endDay.value
+    var result = 0;
+    if(moment(startDay).isSame(endDay, 'month')){
+       let targetMonth = findIndex(this.props.budgets,'month',startDay.substring(0,startDay.lastIndexOf('-')))
+       if(targetMonth && targetMonth.amount){
+         result = targetMonth.amount
+       }
+    }else{
+      this.props.budgets.forEach((budget, index) =>{
+         let daysInMonth = moment(budget.month, "YYYY-MM").daysInMonth()
+         if(moment(budget.month).isSame(startDay, 'month') ){
+           let daysLeft = daysInMonth - moment(startDay).date()+1
+           result += budget.amount*(daysLeft/daysInMonth)
+         }else if(moment(budget.month).isSame(endDay, 'month')){
+           let daysLeft = moment(endDay).date()
+           result += budget.amount*(daysLeft/daysInMonth)
+         }else if(moment(budget.month).isBetween(startDay,endDay)){
+           result += budget.amount
+         }
+      })
+    }
+    this.setState({total:result})
   }
   render() {
     const {goToAddBudget, budgets} = this.props
     return (
       <Card>
+        <input type='text' ref='startDay' />
+        <input type='text' ref='endDay' />
+        <button onClick={() => this.caculate()}>Caculate</button>
+        <span>result:</span>
+        <span>{this.state.total}</span>
         <CardTitle title='Budgets'/>
         <CardText>
           <Table height='500px' fixedHeader={true} >
@@ -52,21 +91,3 @@ function mapStateToProps(state){
 function mapDispatchToProps(dispatch){
   return bindActionCreators(merge({}, BudgetActions, NavigationActions), dispatch)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
